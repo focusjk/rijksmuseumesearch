b53eec9b59edfd3dc836863ec25192cfe3549d28
Admin <admin@MacBook-Pro-khxng-Admin.local>
2018-05-31 11:56:56 +0700

    change css to style component 1

--- src/pictureResult.js
+++ src/pictureResult.js
@@ -2,40 +2,53 @@ import './style.css';
 
 import React, { Component } from 'react';
 
+import { Card } from './style';
+import styled from 'styled-components';
+
 class PictureResult extends Component {
     constructor(props){
         super(props)
     }
     render() {
         return(
-            <div className="picturePanel" >
-                <div className="headerPicture">
-                    { this.props.keyword } 
-                    { this.props.loading && <div style = {{ fontSize: '24px', display: 'inline'}}> ( loading )</div> }
-                    { !this.props.loading && this.props.keyword &&
-                        <div style = {{ fontSize: '24px', display: 'inline'}}> 
-                            { this.props.result.length>1? ' ( ' + this.props.result.length + ` results )` : ' ( ' + this.props.result.length + ` result )`}
-                        </div> 
-                    }
-                </div> 
-                {
-                    this.props.result.map((item) => {
-                        return (
-                            
-                            <div className="pictureItem">
-                                <div className="pictureDetail">
-                                    <div className="detailLine">title: {item.title? item.title: '-'}</div>
-                                    <div className="detailLine">production place: {item.productionPlaces.length>0? item.productionPlaces.join(','): '-'}</div>
-                                    <div className="detailLine">principal: {item.principalOrFirstMaker? item.principalOrFirstMaker: '-'}</div>
-                                    <a href={item.links.web} className="detailLink">...More...</a>
+            <Card  backgroundColor="rgb(255,212,159)">
+                <div className="picturePanel">
+                    <div className="headerPicture">
+                        { this.props.keyword } 
+                        { this.props.loading && <div style = {{ fontSize: '24px', display: 'inline'}}> ( loading )</div> }
+                        { !this.props.loading && this.props.keyword &&
+                            <div style = {{ fontSize: '24px', display: 'inline'}}> 
+                                { this.props.result.length>1? ' ( ' + this.props.result.length + ` results )` 
+                                : ' ( ' + this.props.result.length + ` result )`}
+                            </div> 
+                        }
+                    </div> 
+                    {
+                        this.props.result.map((item) => {
+                            return (
+                                
+                                <div className="pictureItem">
+                                    <div className="pictureDetail">
+                                        <div className="detailLine">title: {item.title? item.title: '-'}</div>
+                                        <div className="detailLine">
+                                            production place: {item.productionPlaces.length>0? item.productionPlaces.join(','): '-'}
+                                        </div>
+                                        <div className="detailLine">
+                                            principal: {item.principalOrFirstMaker? item.principalOrFirstMaker: '-'}
+                                        </div>
+                                        <a href={item.links.web} className="detailLink">...More...</a>
+                                    </div>
+                                    <img 
+                                        className="picture" 
+                                        src={item.webImage && item.webImage.url? item.webImage.url
+                                            : 'https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/512x512/plain/sign_forbidden.png'} 
+                                        />
                                 </div>
-                                <img className="picture" src={item.webImage && item.webImage.url? item.webImage.url: 'https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/512x512/plain/sign_forbidden.png'} />
-                            </div>
-                        );
-                    })
-                }
-
-            </div>
+                            );
+                        })
+                    }
+                </div>
+            </Card>
         );
     }
 }
--- src/searchPanel.js
+++ src/searchPanel.js
@@ -2,6 +2,29 @@ import './style.css';
 
 import React, { Component } from 'react';
 
+import { Card } from './style';
+import styled from 'styled-components';
+
+const Input = styled.input`
+    font-size: 16px;
+    border-radius: 5px;
+    border: 0px;
+    padding-left: 16px; 
+    margin: 8px;
+    width: 40%;
+    font-family: Courier;
+`;
+const Button = styled.button`
+    font-size: 16px;
+    border-radius: 5px;
+    border: 0px;
+    padding: 8px 16px 8px 16px;
+    margin: 8px 0 8px 18px;
+    background-color: rgb(150,97,95);
+    color: white;
+    font-family: Courier;
+`;
+    
 class SearchPanel extends Component {
     constructor(props){
         super(props);
@@ -20,17 +43,19 @@ class SearchPanel extends Component {
         }
     }
     
-
     render() {
         return(
-            <div className="searchPanel">
-                <input className="searchInput" type="text" ref="keyword" placeholder="ex. puppy" onKeyDown={(e) => this.onKeyDown(e)}>
-                </input>
-
-                <button className="searchButton" onClick={() => this.sendKeyword()}>
+            <Card backgroundColor="rgba(227,180,170,1)" >
+                <input 
+                    type="text" 
+                    ref="keyword" 
+                    placeholder="ex. puppy" 
+                    onKeyDown={(e) => this.onKeyDown(e)} 
+                />
+                <Button onClick={() => this.sendKeyword()}>
                     Search!
-                </button>
-            </div>
+                </Button>
+            </Card>
         );
     }
 }
